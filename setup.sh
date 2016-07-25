7620e204a397bc89fb8a9d18b353bb357b678d9a
Michael Aquilina <michaelaquilina@gmail.com>
2016-07-25 23:10:04 +0100

    Set to python2.7 due to compatability reasons

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 if [ -n "$VIRTUAL_ENV" ]; then
   pip install -r requirements.txt
   echo "Symlinking gi module to $VIRTUAL_ENV"
-  ln -s "/usr/lib/python3/dist-packages/gi" "$VIRTUAL_ENV/lib/python3.5"
+  ln -s "/usr/lib/python2.7/dist-packages/gi" "$VIRTUAL_ENV/lib/python2.7"
 else
   echo "Not currently in a virtualenv! Change to a virtualenv and re-run this script"
 fi
